241e3149590724edae1d079e8c1d3eaa8d2e43a5
= <bbadrunn@gmail.com>
2022-01-22 19:21:47 +0700

    update

--- config/production.ts
+++ config/production.ts
@@ -2,10 +2,8 @@ export default {
   port: process.env.PORT || 4000,
   db_url: process.env.MONGO_URL || 'mongodb://localhost:27017/ts-node',
   saltWorkFactor: 10,
-  privateKey:
-    'thisissecretkey',
-  publicKey:
-    'thisissecretkey',
+  privateKey: 'thisissecretkey',
+  publicKey: 'thisissecretkey',
   accessTokenTtl: '15m',
   refresToken: '1y',
 };
